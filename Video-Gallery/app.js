c8bf34b199b0463f7c61233c8fd7c52075e508fc
Abiagomba <63677871+Abiagomba@users.noreply.github.com>
2023-11-25 09:18:45 -0500

    Add files via upload

---
+++ Video-Gallery/app.js
@@ -0,0 +1,16 @@
+// let listVideo = document.querySelectorAll('.video-list .vid')
+// let mainVideo = document.querySelector('.main-video video')
+// let tit = document.querySelector('.main-video .title')
+
+// listVideo.forEach(video => {
+//     video.onClick = () => {
+//         listVideo.forEach(vid => vid.classList.remove('active'))
+//         video.classList.add('active')
+//         if (video.classList.contains('active')) {
+//             let src = video.children[0].getAttribute('src')
+//             mainVideo.src = src
+//             let text = video.children[1].innerHTML
+//             tit.innerHTML = text
+//         }
+//     }
+// })
